3ca77e7eb138de92cb2b99b49b348be8f05daae7
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-09-05 13:36:05 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import {
   Route
 } from 'react-router-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import { App, Shop, Blog, About } from './App';
 import reportWebVitals from './reportWebVitals';
 
